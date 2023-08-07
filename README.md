65e0e29d812e90b7097f6d0f807cd05b0d9491ab
laixiangyong <1809925184@qq.com>
2023-08-07 18:37:12 +0800

    测试脚手架

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: Lai
+ * @Date: 2023-08-07 16:33:32
+ * @LastEditors: Lai
+ * @LastEditTime: 2023-08-07 16:45:43
+-->
 # vue_test
 
 ## Project setup
@@ -22,3 +30,4 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
---
+++ src/components/Student.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: Lai
+ * @Date: 2023-08-07 13:22:49
+ * @LastEditors: Lai
+ * @LastEditTime: 2023-08-07 13:54:17
+-->
+ <!-- 组件的结构 -->
+<template>
+  <div>
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+    
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码(数据、方法等)
+// 分别暴露
+export default {
+  name:`Student`,
+  data() {
+    return {
+      name: "张三",
+      age:19,
+    };
+  },
+};
+// 统一暴露
+// export{school}
+// 默认暴露
+// export default school
+</script>
