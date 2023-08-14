b934bfcbdf09252012e9341b7ab5ecf79a209882
laixiangyong <1809925184@qq.com>
2023-08-14 14:49:45 +0800

    新增

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -11,6 +11,7 @@
     <h2>学生姓名：{{ name }}</h2>
     <h2>学生性别：{{ sex }}</h2>
     <button @click="sendStudentName">把学生名给APP</button>
+    <button @click="unbind">解绑lai事件</button>
   </div>
 </template>
 
@@ -27,6 +28,9 @@ export default {
     sendStudentName(){
       // 触发Student组件实例身上的lai事件
       this.$emit('lai',this.name,12,345,6,5)
+    },
+    unbind(){
+      
     }
   }
 };
