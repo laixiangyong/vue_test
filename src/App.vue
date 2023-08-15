e69240bb8c40bc3165d24ac1368ea2a1f92fa3d2
laixiangyong <1809925184@qq.com>
2023-08-15 09:15:30 +0800

    自定义组件事件

--- src/App.vue
+++ src/App.vue
@@ -4,53 +4,61 @@
  * @Autor: Lai
  * @Date: 2023-08-08 08:35:39
  * @LastEditors: Lai
- * @LastEditTime: 2023-08-14 14:39:13
+ * @LastEditTime: 2023-08-14 16:52:01
 -->
 <template>
   <div class="app">
-    <h1>{{msg}}</h1>
+    <h1>{{ msg }}，学生姓名是：{{studentName}}</h1>
     <!-- 通过父组件给子组件传递函数类型的props实现：子给父传递数据 -->
-    <School :getSchoolName="getSchoolName"/>
-     <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第一种写法） -->
-    <Student @lai="getStudentName"/>
+    <School :getSchoolName="getSchoolName" />
+    <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第一种写法） -->
+    <!-- <Student @lai="getStudentName" @demo="m1" /> -->
     <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第二种写法，使用ref） -->
-    <!-- <Student ref="student"/> -->
+    <Student ref="student" @click.native="show"/>
   </div>
 </template>
 
 <script>
-
-import Student from './components/Student.vue'
-import School from './components/School.vue'
+import Student from "./components/Student.vue";
+import School from "./components/School.vue";
 export default {
-    name:'App',
-     components:{
-        Student,
-        School
+  name: "App",
+  components: {
+    Student,
+    School,
+  },
+  data() {
+    return {
+      msg: "真不错！",
+      studentName:''
+    };
+  },
+  methods: {
+    getSchoolName(name) {
+      console.log("app收到了学校名", name);
     },
-    data() {
-      return {
-        msg:'真不错！'
-      }
+    getStudentName(name, ...params) {
+      console.log("APP收到了学生姓名", name, params);
+      this.studentName = name
     },
-    methods:{
-      getSchoolName(name){
-        console.log('app收到了学校名',name);
-      },
-      getStudentName(name,...params){
-        console.log('APP收到了学生姓名',name,params);
-       } 
+    m1(){
+      console.log('demo事件被触发了');
+      
     },
-    mounted() {
-      // this.$refs.student.$on('lai',this.getStudentName)//绑定自定义事件
-      // this.$refs.student.$once('lai',this.getStudentName)//绑定自定义事件（一次性）
-    },
-}
+    show(){
+      alert('真不错')
+    }
+  },
+  mounted() {
+    this.$refs.student.$on('lai',this.getStudentName)//绑定自定义事件
+    // this.$refs.student.$once('lai',this.getStudentName)//绑定自定义事件（一次性）
+  },
+};
 </script>
  
 <style  scoped>
-  .app{
-    background-color: darkgrey;
-     padding: 5px;
-  }
+.app {
+  background-color: darkgrey;
+  padding: 5px;
+}
 </style>
--- src/components/Student.vue
+++ src/components/Student.vue
@@ -4,14 +4,17 @@
  * @Autor: Lai
  * @Date: 2023-08-08 08:42:35
  * @LastEditors: Lai
- * @LastEditTime: 2023-08-14 13:50:54
+ * @LastEditTime: 2023-08-14 16:56:26
 -->
 <template>
   <div class="student">
     <h2>学生姓名：{{ name }}</h2>
     <h2>学生性别：{{ sex }}</h2>
+    <h2>当前求和为：{{number}}</h2>
+    <button @click="add">点我number++</button>
     <button @click="sendStudentName">把学生名给APP</button>
     <button @click="unbind">解绑lai事件</button>
+    <button @click="death">销毁当前student组件实例（vc）</button>
   </div>
 </template>
 
@@ -22,15 +25,27 @@ export default {
     return {
       name: "李四",
       sex: "男",
+      number:0
     }
   },
   methods:{
+    add(){
+      console.log('add调用了');
+      this.number++
+    },
     sendStudentName(){
       // 触发Student组件实例身上的lai事件
       this.$emit('lai',this.name,12,345,6,5)
+      // this.$emit('demo')
+      // this.$emit('click')
     },
     unbind(){
-      
+      this.$off('lai')//解绑一个自定义事件
+      // this.$off(['lai','demo'])//解绑多个自定义事件
+      // this.$off()//解绑所有自定义事件
+    },
+    death(){
+      this.$destroy()//销毁了当前Student组件实例
     }
   }
 };
