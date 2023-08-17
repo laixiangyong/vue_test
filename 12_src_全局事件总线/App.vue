fc1603cf2b322d2180d4d2c4180dfc7399779608
laixiangyong <1809925184@qq.com>
2023-08-17 08:16:55 +0800

    新增全局事件总线

---
+++ 12_src_全局事件总线/App.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: Lai
+ * @Date: 2023-08-08 08:35:39
+ * @LastEditors: Lai
+ * @LastEditTime: 2023-08-16 13:56:40
+-->
+<template>
+  <div class="app">
+    <h1>{{ msg }}</h1>
+    <School/>
+    <Student/>
+  </div>
+</template>
+
+<script>
+import Student from "./components/Student.vue";
+import School from "./components/School.vue";
+export default {
+  name: "App",
+  components: {
+    Student,
+    School,
+  },
+  data() {
+    return {
+      msg: "真不错！",
+    };
+  },
+};
+</script>
+ 
+<style  scoped>
+.app {
+  background-color: darkgrey;
+  padding: 5px;
+}
+</style>
--- 12_src_全局事件总线/main.js
+++ 12_src_全局事件总线/main.js
@@ -7,8 +7,12 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 
+
 // 创建vm
 new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(App),
+    beforeCreate(){
+        Vue.prototype.$bus = this//安装全局事件总线
+    }
 })
