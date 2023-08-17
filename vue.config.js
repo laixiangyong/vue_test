a6b89d8a157e3062dc31369b168e71eab28b82ae
laixiangyong <1809925184@qq.com>
2023-08-17 16:26:31 +0800

    新增配置代理服务器

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@
  * @Autor: Lai
  * @Date: 2023-08-07 16:53:15
  * @LastEditors: Lai
- * @LastEditTime: 2023-08-07 19:09:47
+ * @LastEditTime: 2023-08-17 16:17:45
  */
 /* const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
@@ -19,5 +19,26 @@ module.exports = {
      
     },
   },
-  lintOnSave:false  // 修改成false 关闭eslint检查
+  lintOnSave:false,  // 修改成false 关闭eslint检查
+  // 开启代理服务器，方式一
+  /* devServer: {
+    proxy: 'http://localhost:5000'
+  } */
+  // 开启代理服务器，方式二
+  devServer: {
+    proxy: {
+      '/lai': {
+        target: 'http://localhost:5000',
+        pathRewrite:{'^/lai':''},
+        // ws: true,//用于支持websocket
+        // changeOrigin: true //用于控制请求头中的host值
+      },
+      '/demo': {
+        target: 'http://localhost:5001',
+        pathRewrite:{'^/demo':''},
+        // ws: true,//用于支持websocket
+        // changeOrigin: true //用于控制请求头中的host值
+      },
+    }
+  }
 }
