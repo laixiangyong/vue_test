7e3d8565b19378cf923abe627202615532e2774d
laixiangyong <1809925184@qq.com>
2023-08-22 16:46:42 +0800

    新增命名路由

---
+++ 33_src_命名路由/pages/Detail.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: Lai
+ * @Date: 2023-08-22 16:08:49
+ * @LastEditors: Lai
+ * @LastEditTime: 2023-08-22 16:08:56
+-->
+<template>
+  <ul>
+    <li>消息编号:{{$route.query.id}}</li>
+    <li>消息标题:{{$route.query.title}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+    name:'Detail',
+
+}
+</script>
